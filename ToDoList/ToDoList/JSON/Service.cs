4f383df1aca978a2ceb18ff0758d0b4c0ae6dbe9
Uczeń <Ucze@S50-04>
2024-01-23 14:29:29 +0100

    add json service and task model

---
+++ ToDoList/ToDoList/JSON/Service.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ToDoList.JSON
+{
+    public static class Service
+    {
+        static string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "baza.json");
+        public static void WriteToFile(List<Task> list)
+        {
+            if (!File.Exists(path))
+                File.WriteAllText(path, "");
+            string json = JsonSerializer.Serialize(list);
+            File.WriteAllText(path, json);
+        }
+        public static List<Task> GetFromFile()
+        {
+            if (!File.Exists(path))
+                File.WriteAllText(path, "");
+            string text = File.ReadAllText(path);
+            if (text != "")
+                return JsonSerializer.Deserialize<List<Task>>(text);
+            return new List<Task>();
+        }
+    }
+}
